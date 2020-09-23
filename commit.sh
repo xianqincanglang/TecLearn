dda68749a076d6743c548e0fc2b0eb15fc97a6e6
chentianyu <chentianyu@qiyi.com>
2020-09-23 18:16:15 +0800

    dev:增加一个提交脚本
    Signed-off-by: chentianyu <chentianyu@qiyi.com>

--- commit.sh
+++ commit.sh
@@ -19,6 +19,14 @@ git push origin master
 
 #switch branch
 git checkout gh-pages
-cp ../_book/ ./
+cp -R ../_book/ ../
+rm -r ../_book
 
+git add .
+git commit -m $(date "+%Y%m%d-%H%M%S")
+git push
+
+
+#switch master branch
+git checkout master
 
