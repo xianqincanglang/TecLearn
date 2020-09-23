6a8727b62162064c89e7309f1a4b18f785cd0f61
chentianyu <chentianyu@qiyi.com>
2020-09-23 18:07:54 +0800

    20200923-180754

---
+++ commit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+git stash clear
+git stash
+git pull origin master
+git stash apply stash@{0}
+
+#run latest gitbook
+gitbook serve
+
+#cp _book到父目录
+cp -r ./_book ../
+
+#commit alters
+
+git add .
+git commit -m $(date "+%Y%m%d-%H%M%S")
+git push origin master
+
+#switch branch
+git checkout gh-pages
+cp ../_book/ ./
+
+
